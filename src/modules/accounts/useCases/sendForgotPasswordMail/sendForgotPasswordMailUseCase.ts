e48150f1a40938fa25494790c15db3dc14f50083
Lucas <lucas>
2022-12-06 10:33:24 -0300

    configurationAWS

--- src/modules/accounts/useCases/sendForgotPasswordMail/sendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/sendForgotPasswordMailUseCase.ts
@@ -17,7 +17,7 @@ class SendForgotPasswordMailUseCase {
     private tokensRepository: IUsersTokensRepository,
     @inject("DayjsDateProvider")
     private dateProvider: IDateProvider,
-    @inject("EtherealMailProvider")
+    @inject("MailProvider")
     private mailProvider: IMailProvider
   ) {}
 
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,16 +1,3 @@
-import { container } from "tsyringe";
-
-import { IDateProvider } from "./dateProvider/IDateProvider";
-import { DayjsDateProvider } from "./dateProvider/implementations/DayjsDateProvider";
-import { IMailProvider } from "./mailProvider/IMailProvider";
-import { EtherealMailProvider } from "./mailProvider/implementations/EtherealMailProvider";
-
-container.registerSingleton<IDateProvider>(
-  "DayjsDateProvider",
-  DayjsDateProvider
-);
-
-container.registerInstance<IMailProvider>(
-  "EtherealMailProvider",
-  container.resolve(EtherealMailProvider)
-);
+import "./dateProvider";
+import "./mailProvider";
+import "./storageProvider";
